063eff9876be29ab99efc0a7dd0b3169457bf904
FABIO JUNIOR RIBEIRO <37555641+frib3iro@users.noreply.github.com>
2022-03-02 12:39:05 -0300

    Add files via upload

--- ssh.sh
+++ ssh.sh
@@ -29,5 +29,5 @@ echo
 echo -e "${S} ${C}Copie o ip para fazer a conexão entre as máquinas${F}"
 ip address show
 
-echo -e "${S} ${G}Para se conectar digite >>> ssh usuario@IP <<<${F}"
+echo -e "${S} ${G}Para se conectar digite${F} ${R}[${F}${C}ssh usuario@IP${F}${R}]${F}"
 
